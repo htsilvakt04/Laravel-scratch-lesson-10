aee737fb260ea40516b022a64c73b8a68d482a40
Trung-Silva <htsilvakt04@gmail.com>
2017-01-03 21:57:11 +0700

    finish lesson 11

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -5,6 +5,15 @@
 
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
+      <!-- Checking form field -->
+      @if (count($errors))
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li style="color: salmon; list-style-type: none "> {{ $error }} </li>
+          @endforeach
+        </ul>
+      @endif
+      <!-- End checking form -->
       <h1>{{ $card->title }}</h1>
       <ul class="list-group">
         @foreach ($card->notes as $note)
@@ -16,9 +25,11 @@
       </ul>
       <hr>
       <h2>Add a new note</h2>
+
       <form class="" action="/cards/{{$card->id}}/notes" method="POST">
+        {{ csrf_field() }}
         <div class="form-group">
-          <textarea name="body" class="form-control"></textarea>
+          <textarea name="body" class="form-control">{{ old("body") }}</textarea>
         </div>
         <button type="submit" class="btn btn-warning">Add Note</button>
       </form>
--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,13 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/w
 
-//
-// Route::get("/", "PagesController@home");
-// Route::get("/about", "PagesController@about");
+Route::group(["middleware" => ["web"]], function() {
+  Route::get("cards", "CardsController@index");
+  Route::get("cards/{card}", "CardsController@show");
 
-Route::get("cards", "CardsController@index");
-Route::get("cards/{card}", "CardsController@show");
-
-Route::post("cards/{card}/notes", "NotesController@store");
-Route::get("notes/{note}/edit", "NotesController@edit");
-Route::patch("notes/{note}", "NotesController@update");
+  Route::post("cards/{card}/notes", "NotesController@store");
+  Route::get("notes/{note}/edit", "NotesController@edit");
+  Route::patch("notes/{note}", "NotesController@update");
+});
